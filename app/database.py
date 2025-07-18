8e1d265c0eceecf380dea9c32d6eb72338b08d5d
KIM SHINEDAY <kimshineday99@gmail.com>
2025-07-18 09:54:11 +0900

    🎉 Create project fastapi-diary
    FastAPI 미니 프로젝트 개발환경 구축

---
+++ app/database.py
@@ -0,0 +1,28 @@
+from tortoise.models import Model
+from tortoise import Tortoise
+
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+TORTOISE_ORM = {
+    "connections": {"default":DATABASE_URL},
+    "apps": {
+        "models": {
+            "models": ["app.models", "aerich.models"],
+            "default_connection": "default",
+        },
+    }
+
+}
+
+async def init_db():
+    await Tortoise.init(config=TORTOISE_ORM)
+    await Tortoise.generate_schemas()
+
+async def close_db():
+    await Tortoise.close_connections()
+
---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.database import init_db, close_db
+
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    await init_db
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await close_db
+
+
+if __name__ == "__main__":
+    main()
