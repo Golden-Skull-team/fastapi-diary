c7582a956bea1d223bea77e1902738a52e71e912
yoo-sh-96 <tmdguq90051@gmail.com>
2025-07-22 17:47:40 +0900

    ✅ Add diary router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.api.user import router as user_router
+from app.api.diarys import diary_router
 from app.database import init_db, close_db
 
 @asynccontextmanager
@@ -12,3 +13,4 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(user_router)
+app.include_router(diary_router)
