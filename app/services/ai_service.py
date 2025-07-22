72bda4eca891e8b0546632793df6b63cd9ee784b
KIM SHINEDAY <kimshineday99@gmail.com>
2025-07-22 14:01:25 +0900

    🔀 브랜치 병합을 위한 준비

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -19,15 +19,15 @@ from google import genai
 from app.core.config import Settings
 import prompt
 
-# client = genai.Client(api_key=api_key)
-genai.configure(api_key=Settings.GEMENI_API_KEY)
-gemini_model = genai.GenerativeModel('gemini-2.5-flash')
+client = genai.Client(api_key=Settings.GEMINI_API_KEY)
+
 
 # 요약
 def diary_request():
     # request = prompt.request_prompt()
     question = prompt.req_t()    
-    response = gemini_model.models.generate_content(
+    response = client.models.generate_content(
+        model = 'gemini-2.5-flash',
         contents=question
     )
     print(response)
