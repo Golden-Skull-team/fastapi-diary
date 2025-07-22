a902316f3daf29578db40255296d98cfd5818767
KIM SHINEDAY <kimshineday99@gmail.com>
2025-07-22 13:15:20 +0900

    🧪 과연 이거 될까?

--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -15,18 +15,19 @@ class CreateDiary(BaseModel):
 
 from google import genai
 # from app.core.gemini_connect import api_key
-from gemini_connect import api_key
+# from gemini_connect import api_key
+from app.core.config import Settings
 import prompt
 
-client = genai.Client(api_key=api_key)
-
+# client = genai.Client(api_key=api_key)
+genai.configure(api_key=Settings.GEMENI_API_KEY)
+gemini_model = genai.GenerativeModel('gemini-2.5-flash')
 
 # 요약
 def diary_request():
     # request = prompt.request_prompt()
     question = prompt.req_t()    
-    response = client.models.generate_content(
-        model='gemini-2.5-flash',
+    response = gemini_model.models.generate_content(
         contents=question
     )
     print(response)
