627e14822a37f6ecebe35e290e5cffb88be9c0c9
00475-AEDF-52510-2 <d050586458@gmail.com>
2025-07-23 11:50:11 +0900

    unique del

--- app/models/token_blacklist.py
+++ app/models/token_blacklist.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class TokenBlacklist(Model):
     id = fields.IntField(pk=True)
-    token = fields.TextField(unique=True)
+    token = fields.CharField(max_length=500, unique=True)
     blacklisted_at = fields.DatetimeField(auto_now_add=True)
 
     class Meta:
